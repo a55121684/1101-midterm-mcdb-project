27276d95115c56ddaa0ce3c3fe7a644da5307d5d
zous71628 <zous71628@gmail.com>
2021-10-23 19:37:27 +0800

    first commit

---
+++ services/serviceController.js
@@ -0,0 +1,10 @@
+const Category =require('../models/Category');
+
+exports.getCategories = async (req, res) =>{
+    try{
+        return await Category.fetchAll(); 
+
+    }catch(err){
+        console.log('getCategories',err);
+    }
+}
