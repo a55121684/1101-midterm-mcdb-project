bab603c95e8f479f801ef783a6bbb229210d0e73
zous71628 <zous71628@gmail.com>
2021-10-23 19:38:42 +0800

    save

---
+++ controllers/apiController.js
@@ -0,0 +1,11 @@
+const serviceController = require('../serviceController');
+
+exports.getCategories = async (req, res) => {
+  try {
+    const data = await serviceController.getCategories();
+    return res.json(data);
+    
+  }catch (err) {
+    console.log('Controller getCategories', err);
+  }
+};
