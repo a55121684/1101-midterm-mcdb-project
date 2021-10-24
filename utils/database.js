6b9759105fb55e3a6dece0b81768be7b279fd7f2
zous71628 <zous71628@gmail.com>
2021-10-25 05:18:02 +0800

    save

--- utils/database.js
+++ utils/database.js
@@ -10,9 +10,9 @@ if (isProduction) {
 } else {
   pool = new Pool({
     user: 'postgres',
-    host: 'ec2-52-71-241-37.compute-1.amazonaws.com',
+    host: 'localhost',
     database: 'd7qtraoo3tsfjb',
-    password: '829705a2c8ffbf6b583900ab67a015ee3616adeb68dfdf4c9fe6fb783b696335',
+    password: '0000',
     port: '5432',
   });
 }
