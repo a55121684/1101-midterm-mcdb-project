80fce64c116071d1973ecbbc3a42313b79c78b49
zous71628 <zous71628@gmail.com>
2021-10-25 11:32:27 +0800

    save

--- routes/api.js
+++ routes/api.js
@@ -1,10 +1,11 @@
 var express = require('express');
 var router = express.Router();
+var router2 = express.Router();
 const apiController = require('../controllers/apiController');
 
 /* GET home page. */
 router.get('/category',apiController.getCategories);
 
-router.get('/category2',apiController.getCategories);
+router2.get('/category2',apiController.getCategories);
 
 module.exports = router;
